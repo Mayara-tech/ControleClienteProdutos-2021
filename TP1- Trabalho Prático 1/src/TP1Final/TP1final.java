af15f59141b5cae0b6d3eb026d7105a3944cbae4
Mayara-tech <67807684+Mayara-tech@users.noreply.github.com>
2022-01-20 17:43:16 -0300

    Update TP1final.java

--- TP1- Trabalho Prático 1/src/TP1Final/TP1final.java
+++ TP1- Trabalho Prático 1/src/TP1Final/TP1final.java
@@ -1,5 +1,5 @@
-		// TP1 - 30.07.2021
-		// Mayara Alves de Oliveira - 200025058
+// Mayara Alves de Oliveira - 200025058
+// Projeto nÃ£o finalizado - apresentando bugs
 
 //		                                  CONTROLE DE CLIENTES E PRODUTOS
 
@@ -13,53 +13,53 @@ public class TP1final {
 	public static void main(String[] args) {
 
 
-				//                                            Menu de Opções
+				//                                            Menu de OpÃ§Ãµes
 
-		System.out.println(" ------------------ MENU DE OPÇÕES ---------------------- \n ");
+		System.out.println(" ------------------ MENU DE OPÃ‡Ã•ES ---------------------- \n ");
 
 		
-				int  MenudeOpções;
+				int  MenudeOpÃ§Ãµes;
 				Scanner input = new Scanner(System.in);
-				System.out.println("Escolha a opção desejada: \n\n1. Realizar cadastro de novo Cliente \n2. Consultar Cliente ja cadastrado \n3. Realizar cadastro de novo produto \n4. Consultar produto ja cadastrado \n5. Cadastro de venda \n6. Consultar produtos em estoque \n0. Sair");    
-				MenudeOpções = input.nextInt();
+				System.out.println("Escolha a opÃ§Ã£o desejada: \n\n1. Realizar cadastro de novo Cliente \n2. Consultar Cliente ja cadastrado \n3. Realizar cadastro de novo produto \n4. Consultar produto ja cadastrado \n5. Cadastro de venda \n6. Consultar produtos em estoque \n0. Sair");    
+				MenudeOpÃ§Ãµes = input.nextInt();
 
-				//                                    Criando as variaveis de opções
+				//                                    Criando as variaveis de opÃ§Ãµes
 
 				int NovoCliente = 0;
-				int NovoEndereço = 0;
+				int NovoEndereÃ§o = 0;
 				int ConsultarCliente = 0;
 				int NovoProduto = 0;
 				int ConsultarProduto = 0;
 				int CadastroVenda = 0;
 				int ProdutosEstoque = 0;
 				int Sair = 0;
-				int Opções = 0;
+				int OpÃ§Ãµes = 0;
 
-				//                                   lista de Clientes/Produtos pré-cadastrados
+				//                                   lista de Clientes/Produtos prÃ©-cadastrados
 
 				String [] NomeClientes = new String[100];
 				NomeClientes[0] = "Mayara Alves de Oliveira";
 				NomeClientes[1] = "Matheus Alves de Oliveira";
-				NomeClientes[2] = "Maria José Alves de Oliveira";
+				NomeClientes[2] = "Maria JosÃ© Alves de Oliveira";
 				NomeClientes[3] = "Francisco Chaves de Oliveira";
 				NomeClientes[4] = "Igor Luna Almeida";
-				NomeClientes[5] = "Vitória Luna Almeida";
+				NomeClientes[5] = "VitÃ³ria Luna Almeida";
 				NomeClientes[6] = "Tatiane Luna ALmeida";
-				NomeClientes[7] = "Natália Gomes Veras";
-				NomeClientes[8] = "João Antonio Nobrega";
+				NomeClientes[7] = "NatÃ¡lia Gomes Veras";
+				NomeClientes[8] = "JoÃ£o Antonio Nobrega";
 				NomeClientes[9] = "Janaina Ferreira Silva";
 
-				String [] EndereçoClientes = new String[100];
-				EndereçoClientes[0] = "Quadra 16 Chácaras 16 lote 01 - Valparaiso";
-				EndereçoClientes[1] = "Quadra 32 Chácaras 01 lote 02 - Novo Gama";
-				EndereçoClientes[2] = "Quadra 04 Chácaras 92 lote 03 - Pedregal";
-				EndereçoClientes[3] = "Quadra 19 Chácaras 02 lote 04 - Luziânia";
-				EndereçoClientes[4] = "Quadra 01 Chácaras 23 lote 05 - Planaltina";
-				EndereçoClientes[5] = "Quadra 22 Chácaras 12 lote 06 - Asa Sul";
-				EndereçoClientes[6] = "Quadra 18 Chácaras 25 lote 07 - Sobradinho";
-				EndereçoClientes[7] = "Quadra 28 Chácaras 11 lote 08 - Taguatinga";
-				EndereçoClientes[8] = "Quadra 37 Chácaras 10 lote 09 - Guará";
-				EndereçoClientes[9] = "Quadra 517 Chácaras 05 lote 10 - Ceilândia";
+				String [] EndereÃ§oClientes = new String[100];
+				EndereÃ§oClientes[0] = "Quadra 16 ChÃ¡caras 16 lote 01 - Valparaiso";
+				EndereÃ§oClientes[1] = "Quadra 32 ChÃ¡caras 01 lote 02 - Novo Gama";
+				EndereÃ§oClientes[2] = "Quadra 04 ChÃ¡caras 92 lote 03 - Pedregal";
+				EndereÃ§oClientes[3] = "Quadra 19 ChÃ¡caras 02 lote 04 - LuziÃ¢nia";
+				EndereÃ§oClientes[4] = "Quadra 01 ChÃ¡caras 23 lote 05 - Planaltina";
+				EndereÃ§oClientes[5] = "Quadra 22 ChÃ¡caras 12 lote 06 - Asa Sul";
+				EndereÃ§oClientes[6] = "Quadra 18 ChÃ¡caras 25 lote 07 - Sobradinho";
+				EndereÃ§oClientes[7] = "Quadra 28 ChÃ¡caras 11 lote 08 - Taguatinga";
+				EndereÃ§oClientes[8] = "Quadra 37 ChÃ¡caras 10 lote 09 - GuarÃ¡";
+				EndereÃ§oClientes[9] = "Quadra 517 ChÃ¡caras 05 lote 10 - CeilÃ¢ndia";
 
 				String [] TelefoneClientes = new String [100];
 				TelefoneClientes [0] = "(61)992342334";
@@ -98,18 +98,18 @@ public class TP1final {
 				NomeProdutos [8] = "Esfirra de hamburguer";
 				NomeProdutos [9] = "Croissant";
 				
-				String [] DescriçãoProdutos = new String [100];
+				String [] DescriÃ§Ã£oProdutos = new String [100];
 				
-				DescriçãoProdutos [0] ="Salgado de Frango (frito)";
-				DescriçãoProdutos [1] ="Salgado de queijo de presunto (frito)";
-				DescriçãoProdutos [2] ="Salgado de carne (frito)";
-				DescriçãoProdutos [3] ="Salgado de carne com massa especial (frito)";
-				DescriçãoProdutos [4] ="Salgado de frango (assado)";
-				DescriçãoProdutos [5] ="Salgado de carne (assado)";
-				DescriçãoProdutos [6] ="Salgado de queijo com Presunto (assado)";
-				DescriçãoProdutos [7] ="Salgado massa especial de frango (assado)";
-				DescriçãoProdutos [8] ="Salgado de carne de hamburguer e cheelder (assado)";
-				DescriçãoProdutos [9] = "Salgado massa especial sabores (assado)";
+				DescriÃ§Ã£oProdutos [0] ="Salgado de Frango (frito)";
+				DescriÃ§Ã£oProdutos [1] ="Salgado de queijo de presunto (frito)";
+				DescriÃ§Ã£oProdutos [2] ="Salgado de carne (frito)";
+				DescriÃ§Ã£oProdutos [3] ="Salgado de carne com massa especial (frito)";
+				DescriÃ§Ã£oProdutos [4] ="Salgado de frango (assado)";
+				DescriÃ§Ã£oProdutos [5] ="Salgado de carne (assado)";
+				DescriÃ§Ã£oProdutos [6] ="Salgado de queijo com Presunto (assado)";
+				DescriÃ§Ã£oProdutos [7] ="Salgado massa especial de frango (assado)";
+				DescriÃ§Ã£oProdutos [8] ="Salgado de carne de hamburguer e cheelder (assado)";
+				DescriÃ§Ã£oProdutos [9] = "Salgado massa especial sabores (assado)";
 				
 				String [] ValorProdutos = new String [100];
 				
@@ -153,17 +153,17 @@ public class TP1final {
 
 					do {
 						
-						//                                Criando estrutura de decisão
+						//                                Criando estrutura de decisÃ£o
 
-						switch (MenudeOpções) {
-						case 1: Opções = NovoCliente;
+						switch (MenudeOpÃ§Ãµes) {
+						case 1: OpÃ§Ãµes = NovoCliente;
 
-						//                            Opção Realizar Cadastro de Novo Cliente
+						//                            OpÃ§Ã£o Realizar Cadastro de Novo Cliente
 
 						System.out.println(" ------------------ REALIZAR CADASTRO DE NOVO CLIENTE ---------------------- \n ");
 
 						Scanner ler = new Scanner(System.in);
-						System.out.println("Digite a quantidade de clientes novos que você deseja cadastrar: ");
+						System.out.println("Digite a quantidade de clientes novos que vocÃª deseja cadastrar: ");
 						NovoCliente = ler.nextInt();
 
 						if(NovoCliente > 0 ) {
@@ -176,8 +176,8 @@ public class TP1final {
 							for(i=0;i<NovoCliente;i++) {
 								System.out.println("Digite o nome completo do novo cliente a ser cadastrado: ");
 								NomeClientes[i] = ler1.nextLine();
-								System.out.println("Digite o endereço completo do novo cliente a ser cadastrado: ");
-								EndereçoClientes[i] = ler1.nextLine();
+								System.out.println("Digite o endereÃ§o completo do novo cliente a ser cadastrado: ");
+								EndereÃ§oClientes[i] = ler1.nextLine();
 								System.out.println("Digite o telefone com DDD do novo cliente a ser cadastrado: ");
 								TelefoneClientes[i] = ler1.nextLine();
 								System.out.println("Digite o CPF do novo cliente a ser cadastrado: ");
@@ -187,14 +187,14 @@ public class TP1final {
 							for(i=0;i<NovoCliente;i++) {
 								System.out.println("Clientes cadastrados com sucesso! \n ");
 								System.out.println("NOME DO CLIENTE: " + NomeClientes[i]);
-								System.out.println("ENDEREÇO DO CLIENTE: " + EndereçoClientes[i]);
+								System.out.println("ENDEREÃ‡O DO CLIENTE: " + EndereÃ§oClientes[i]);
 								System.out.println("TELEFONE DO CLIENTE: " + TelefoneClientes[i]);
 								System.out.println("CPF DO CLIENTE: " + CPFClientes[i] + "\n");
 
 							}
 
 						} else {
-							System.out.println("Por favor escolha um número Válido! " ); 
+							System.out.println("Por favor escolha um nÃºmero VÃ¡lido! " ); 
 							System.out.println("Programa Encerrado");
 
 						} 
@@ -202,9 +202,9 @@ public class TP1final {
 
 						continue;
 
-						case 2: Opções = ConsultarCliente;
+						case 2: OpÃ§Ãµes = ConsultarCliente;
 
-						//                               Opção Consultar Cliente ja Cadastrado
+						//                               OpÃ§Ã£o Consultar Cliente ja Cadastrado
 
 						System.out.println(" ------------------ CONSULTAR CLIENTE CADASTRADO ---------------------- \n ");
 						String ConsultarCliente1;
@@ -227,15 +227,15 @@ public class TP1final {
 							}
 						}
 						continue;
-						case 3: Opções = NovoProduto;
+						case 3: OpÃ§Ãµes = NovoProduto;
 
-						//                                Opção Realizar Cadastro de Novo Produto
+						//                                OpÃ§Ã£o Realizar Cadastro de Novo Produto
 
 						System.out.println(" ------------------ REALIZAR CADASTRO DE NOVO PRODUTO ---------------------- \n ");
 
 
 						Scanner ler3 = new Scanner(System.in);
-						System.out.println("Digite a quantidade de novos produtos que você deseja cadastrar: ");
+						System.out.println("Digite a quantidade de novos produtos que vocÃª deseja cadastrar: ");
 						NovoProduto = ler3.nextInt();
 
 						if(NovoProduto > 0 ) {
@@ -246,8 +246,8 @@ public class TP1final {
 							for(i=0;i<NovoProduto;i++) {
 								System.out.println("Digite o nome do produto a ser cadastrado: ");
 								NomeProdutos[i] = ler1.nextLine();
-								System.out.println("Digite a descrição do produto a ser cadastrado: ");
-								DescriçãoProdutos [i] = ler1.nextLine();
+								System.out.println("Digite a descriÃ§Ã£o do produto a ser cadastrado: ");
+								DescriÃ§Ã£oProdutos [i] = ler1.nextLine();
 								System.out.println("Digite o valor de compra do produto a ser cadastrado: ");
 								ValorProdutos[i] = ler1.nextLine();
 								System.out.println("Digite a porcentagem de lucro correspondente ao produto a ser cadastrado: ");
@@ -259,7 +259,7 @@ public class TP1final {
 							System.out.println("Produtos cadastrados com sucesso! ");
 							for(i=0;i<NovoProduto;i++) {
 								System.out.println("NOME DO PRODUTO: " + NomeProdutos[i]);
-								System.out.println("DESCRIÇÃO DO PRODUTO: " + DescriçãoProdutos [i]);
+								System.out.println("DESCRIÃ‡ÃƒO DO PRODUTO: " + DescriÃ§Ã£oProdutos [i]);
 								System.out.println("VALOR DO PRODUTO: " + ValorProdutos[i]);
 								System.out.println("LUCRO DO PRODUTO (PORCENTAGEM) " + LucroProdutos[i]);
 								System.out.println("QUANTIDADE EM ESTOQUE DO PRODUTO: " + EstoqueProdutos[i] + "\n");
@@ -267,16 +267,16 @@ public class TP1final {
 
 
 						} else {
-							System.out.println("Por favor escolha um número Válido! " ); 
+							System.out.println("Por favor escolha um nÃºmero VÃ¡lido! " ); 
 							System.out.println("Programa Encerrado");
 
 						} 
 
 
 						continue;
-						case 4: Opções = ConsultarProduto;
+						case 4: OpÃ§Ãµes = ConsultarProduto;
 						
-						//                               Opção Consultar Produto ja Cadastrado
+						//                               OpÃ§Ã£o Consultar Produto ja Cadastrado
 						
 
 						System.out.println(" ------------------ CONSULTAR PRODUTO CADASTRADO ---------------------- \n ");
@@ -299,8 +299,8 @@ public class TP1final {
 							}
 						}
 
-						case 5: Opções = CadastroVenda;
-//		              Opção Cadastro de venda
+						case 5: OpÃ§Ãµes = CadastroVenda;
+//		              OpÃ§Ã£o Cadastro de venda
 						
 						System.out.println(" ------------------ CADASTRO DE VENDA ---------------------- \n ");
 			
@@ -312,35 +312,35 @@ public class TP1final {
 						}
 						continue;
 						
-						case 6: Opções = ProdutosEstoque;
+						case 6: OpÃ§Ãµes = ProdutosEstoque;
 						
-						//                             Opção Consultar Produtos em Estoque
+						//                             OpÃ§Ã£o Consultar Produtos em Estoque
 						
 						System.out.println(" ------------------ CONSULTAR PRODUTOS EM ESTOQUE ---------------------- \n ");
 						
-						System.out.println ("NOME --------- DESCRIÇÃO --------- VALOR --------- LUCRO (%) ------- DISPONÍVEL EM ESTOQUE \n\n");
+						System.out.println ("NOME --------- DESCRIÃ‡ÃƒO --------- VALOR --------- LUCRO (%) ------- DISPONÃVEL EM ESTOQUE \n\n");
 
 						for(i=0; i<10; i++) {
 							
-							System.out.println( NomeProdutos[i] + " ----- " + DescriçãoProdutos [i] + " ----- " + ValorProdutos [i] + " ----- " + LucroProdutos [i] + " ----- " + EstoqueProdutos [i] + "\n");
+							System.out.println( NomeProdutos[i] + " ----- " + DescriÃ§Ã£oProdutos [i] + " ----- " + ValorProdutos [i] + " ----- " + LucroProdutos [i] + " ----- " + EstoqueProdutos [i] + "\n");
 
 						}
 						continue;
-						default: Opções = Sair;
+						default: OpÃ§Ãµes = Sair;
 
-						//                                          Opção Sair
+						//                                          OpÃ§Ã£o Sair
 
 						System.out.println(" ------------------ SAIR ---------------------- \n ");
 
 						Scanner ler1 = new Scanner(System.in);
-						System.out.println("Tem certeza que deseja sair? \n1. Sim \n2. Não");    
+						System.out.println("Tem certeza que deseja sair? \n1. Sim \n2. NÃ£o");    
 						Sair = ler1.nextInt();
 						if(Sair==1) {
 							System.out.println("Programa encerrado");
-							Opções = Sair;
+							OpÃ§Ãµes = Sair;
 						} else {
-							System.out.println("Escolha a opção desejada: \n1. Realizar cadastro de novo Cliente \n2. Consultar Cliente ja cadastrado \n3. Realizar cadastro de novo produto \n4. Consultar produto ja cadastrado \n5. Cadastro de venda \n6. Consultar produtos em estoque \n0. Sair");    
-							MenudeOpções = input.nextInt();
+							System.out.println("Escolha a opÃ§Ã£o desejada: \n1. Realizar cadastro de novo Cliente \n2. Consultar Cliente ja cadastrado \n3. Realizar cadastro de novo produto \n4. Consultar produto ja cadastrado \n5. Cadastro de venda \n6. Consultar produtos em estoque \n0. Sair");    
+							MenudeOpÃ§Ãµes = input.nextInt();
 
 						} 
 
@@ -348,7 +348,7 @@ public class TP1final {
 
 						}
 
-					} while (Opções!=Sair);
+					} while (OpÃ§Ãµes!=Sair);
 
 		}
 
